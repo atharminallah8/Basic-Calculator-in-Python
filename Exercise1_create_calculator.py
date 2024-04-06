0283f2ac0c4732ea02591744d1e23d82188fdbd5
atharminallah8 <145109949+atharminallah8@users.noreply.github.com>
2024-04-06 15:21:36 +0500

    Add files via upload

---
+++ Exercise1_create_calculator.py
@@ -0,0 +1,15 @@
+'''
+------------Exercise 1 - Create a Calculator-------------
+Create a calculator capable of performing addition, subtraction, multiplication and division
+operations on two numbers. Your program should format the output in a readable manner!
+'''
+x = 999
+y = 192843
+add = x + y
+subtract = x - y
+multiply = x * y
+divide = x / y
+print("The addition of ",x," and ",y," is: ",add)
+print("The subtraction of ",x," and ",y," is: ",subtract)
+print("The multiplication of ",x," and ",y," is: ",multiply)
+print("The division of ",x," and ",y," is: ",divide)
